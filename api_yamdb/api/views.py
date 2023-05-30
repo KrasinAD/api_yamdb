d3b6eedcb3fe8e4b117dd8a0c7bbffc8d53187fb
Dmitrii Ounap <ounap2016@yandex.ru>
2023-05-30 23:39:24 +0500

    Reviews модель, прочие правки

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from titles.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import LimitOffsetPagination

def8ca973e6a8b9bdf88c4bd57948e735380c43c
Виктор Шустров <shustrov19@gmail.com>
2023-06-05 20:22:06 +0300

    незначительные исправления

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -11,10 +11,10 @@ class IsAuthorModerAdminOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (
             request.method in permissions.SAFE_METHODS
-            or (obj.author == request.user
-                or request.user.role == 'admin'
-                or request.user.role == 'moderator'
-                or request.user.is_superuser)
+            or obj.author == request.user
+            or request.user.role == 'admin'
+            or request.user.role == 'moderator'
+            or request.user.is_superuser
         )
 
 
